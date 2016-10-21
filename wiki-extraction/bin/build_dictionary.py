03673863e63507866c18c936bffff0e5f3d4c6e7
Andre Lynum <Andre.Lynum@nrk.no>
2016-10-22 01:19:32 +0200

    Filtering options and improved translation speed.

--- wiki-extraction/bin/build_dictionary.py
+++ wiki-extraction/bin/build_dictionary.py
@@ -6,10 +6,11 @@ import logging
 import multiprocessing
 import os
 import re
+import sys
 from argparse import ArgumentParser
 from collections import Counter
+from operator import itemgetter
 
-import sys
 from nltk.tokenize import sent_tokenize
 
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'lib'))
@@ -17,7 +18,7 @@ sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '
 from apertium import translate
 
 
-def articles(wiki_json_fn, limit = None):
+def articles(wiki_json_fn, limit=None):
     count = 0
 
     with io.open(wiki_json_fn, mode='r', encoding='utf-8') as f:
@@ -63,9 +64,10 @@ def compare(tokens, trans_tokens):
 def article_to_pairs(article):
     pairs = []
     sents = sent_tokenize(article['text'], language='norwegian')
+    translations = translate(sents)
 
-    for sent in sents:
-        trans_tokens = tokenize(translate(sent))
+    for sent, trans in zip(sents, translations):
+        trans_tokens = tokenize(trans)
         tokens = tokenize(sent)
 
         pairs += compare(tokens, trans_tokens)
@@ -73,16 +75,65 @@ def article_to_pairs(article):
     return pairs
 
 
-def add_to_map(m, pairs):
-    for a, b in pairs:
-        if a in m:
-            counter = m[a]
-            counter.update([b])
+class TranslationCounter():
+    def __init__(self, source_tf_filter=1, source_df_filter=1.0, trans_tf_filter=1, trans_df_filter=1.0,
+                 top_n=None, print_counts=True):
+        self.source_tf_filter = source_tf_filter
+        self.source_df_filter = source_df_filter
+        self.trans_tf_filter = trans_tf_filter
+        self.trans_df_filter = trans_df_filter
+        self.top_n = top_n
+        self.print_counts = print_counts
+
+        self.count_dict = {}
+        self.source_tf = Counter()
+        self.trans_tf = Counter()
+        self.source_df = Counter()
+        self.trans_df = Counter()
+        self.count_docs = 0
+
+    def _add_to_map(self, pairs):
+        for a, b in pairs:
+            if a in self.count_dict:
+                counter = self.count_dict[a]
+                counter.update([b])
+            else:
+                self.count_dict[a] = Counter([b])
+
+        return self
+
+    def _filtered_trans(self, word):
+        return not ((self.trans_tf[word] >= self.trans_tf_filter) and
+                    (self.trans_df[word] / float(self.count_docs) <= self.trans_df_filter))
+
+    def _format(self, word, count):
+        if self.print_counts:
+            return '%s:%d' % (word, count)
         else:
-            m[a] = Counter([b])
+            return word
 
-    return m
+    def update(self, pairs):
+        self.count_docs += 1
 
+        source_tokens, trans_tokens = zip(*pairs)
+        self.source_tf.update(source_tokens)
+        self.source_df.update(set(source_tokens))
+        self.trans_tf.update(trans_tokens)
+        self.trans_df.update(set(trans_tokens))
+
+        self._add_to_map(pairs)
+
+    def print(self, f):
+        for key, counts in self.count_dict.items():
+            if (self.source_tf[key] >= self.source_tf_filter) and \
+                    (self.source_df[key] / float(self.count_docs) <= self.source_df_filter):
+                candidates = [(v, c) for v, c in counts.items() if not self._filtered_trans(v)]
+                candidates = sorted(candidates, key=itemgetter(1), reverse=True)
+
+                if self.top_n:
+                    candidates = candidates[:self.top_n]
+
+                f.write(u'%s\t%s\n' % (key, ' '.join([self._format(v, c) for v, c in candidates])))
 
 
 def main():
@@ -91,6 +142,12 @@ def main():
     parser.add_argument('-l', '--limit', default=0, type=int)
     parser.add_argument('-i', '--input-file')
     parser.add_argument('-o', '--output-file')
+    parser.add_argument('-s', '--source-df-filter', default=1.0, type=float)
+    parser.add_argument('-t', '--trans-df-filter', default=1.0, type=float)
+    parser.add_argument('-S', '--source-tf-filter', default=1, type=int)
+    parser.add_argument('-T', '--trans-tf-filter', default=1, type=int)
+    parser.add_argument('-n', '--top-n', default=0, type=int)
+    parser.add_argument('-C', '--supress-counts', action='store_true')
     opts = parser.parse_args()
 
     n_procs = opts.procs
@@ -100,14 +157,15 @@ def main():
 
     pool = multiprocessing.Pool(processes=n_procs)
 
-    count_dict = {}
+    trans_counter = TranslationCounter(source_tf_filter=opts.source_tf_filter, source_df_filter=opts.source_df_filter,
+                                       trans_tf_filter=opts.trans_tf_filter, trans_df_filter=opts.trans_df_filter,
+                                       top_n=opts.top_n, print_counts=not opts.supress_counts)
 
     for pairs in pool.map(article_to_pairs, articles(wiki_fn, limit=limit)):
-        add_to_map(count_dict, pairs)
+        trans_counter.update(pairs)
 
     with io.open(out_fn, mode='w', encoding='utf-8') as f:
-        for key, counts in count_dict.items():
-            f.write(u'%s\t%s\n' % (key, ' '.join(['%s:%s' % (v, c) for v, c in counts.items()])))
+        trans_counter.print(f)
 
 
 if __name__ == '__main__':
--- wiki-extraction/lib/apertium.py
+++ wiki-extraction/lib/apertium.py
@@ -1,14 +1,17 @@
 import logging
+import re
 import subprocess
 
 
-def translate(sent):
-    proc = subprocess.Popen(['apertium', 'nno-nob'],
-                           stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def translate(sents):
+    sents = [re.sub(r'\s+', ' ', sent) for sent in sents]
 
-    output, err = proc.communicate(sent.encode('utf-8'))
+    proc = subprocess.Popen(['apertium', 'nno-nob'],
+                            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    output, err = proc.communicate(u'\n'.join(sents).encode('utf-8'))
 
     if len(err) > 0:
         logging.error("apertium returned error: %s" % err.decode('utf-8', errors='ignore'))
 
-    return output.decode('utf-8')
+    return output.decode('utf-8').split(u'\n')
